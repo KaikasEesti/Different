4966b3686103ce1236d74d5e36785c25d2233be7
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2022-02-08 12:57:29 +0300

    first commit

--- src/main/java/ru/netology/Statistic/StatisticService.java
+++ src/main/java/ru/netology/Statistic/StatisticService.java
@@ -1,7 +1,12 @@
 package ru.netology.Statistic;
 
 public class StatisticService {
-
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incimes
+     * @return - index of first max value
+     */
     public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
--- src/test/java/ru/netology/Statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/Statistic/StatisticServiceTest.java
@@ -8,7 +8,7 @@ class StatisticServiceTest {
 
     @Test
     void findMax() {
-        StatisticService service =  new StatisticService();
+        StatisticService service = new StatisticService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
